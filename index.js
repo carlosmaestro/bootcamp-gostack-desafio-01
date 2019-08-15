657ee74fea3bad8f4dddb281cdfdd2b0e5ef9fd3
Carlos Maestro <carlos.maestro@memora.com.br>
2019-08-15 14:30:54 -0300

    desafio completo

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const server = express();
 server.use(express.json());
+server.use(registerRoute);
+
+let countReq = 0;
 
 const projects = [
   {
@@ -27,7 +30,7 @@ server.post('/projects', (req, res) => {
   res.json(projects);
 });
 
-server.put('/projects/:id', (req, res) => {
+server.put('/projects/:id', projectExists, (req, res) => {
   const id = req.params.id;
   const { title } = req.body;
 
@@ -43,7 +46,7 @@ server.put('/projects/:id', (req, res) => {
   res.json(projects[projectIndex]);
 });
 
-server.delete('/projects/:id', (req, res) => {
+server.delete('/projects/:id', projectExists, (req, res) => {
   const id = req.params.id;
 
   let projectIndex = null;
@@ -58,8 +61,42 @@ server.delete('/projects/:id', (req, res) => {
   res.send();
 });
 
+server.post('/projects/:id/tasks', projectExists, (req, res) => {
+  const id = req.params.id;
+  const { title } = req.body;
 
+  let projectIndex = null;
+  projects.map((obj, index) => {
+    if (obj.id == id) {
+      projectIndex = index;
+    }
+  });
 
+  projects[projectIndex].tasks.push(title);
 
+  res.json(projects[projectIndex]);
+});
+
+// Middlewares
+
+function projectExists(req, res, next) {
+  const id = req.params.id;
+  let projectIndex = null;
+  projects.map((obj, index) => {
+    if (obj.id == id) {
+      projectIndex = index;
+    }
+  });
+  if (projectIndex === null) {
+    return res.status(400).json({ error: 'Project does not exists' });
+  }
+
+  next();
+}
+
+function registerRoute(req, res, next) {
+  console.log(`Requisições executadas: ${++countReq}`);
+  return next();
+}
 
 server.listen(3000);
